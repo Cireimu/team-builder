b3cfd72b56dae5f1e4316ae920fab396a211e2a4
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-10-16 16:16:59 -0700

    MVP

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function Form (props) {
+    const [input, setInput] = useState({name: "", email: "", role: ""});
+
+    function handleChange(event) {
+      setInput({ ...input, [event.target.name]: event.target.value });
+      // console.log(event.target.value)
+
+    }
+
+    function submitForm(event) {
+      event.preventDefault();
+      event.target.reset();
+            const newMember = {
+                  ...input,
+              };
+      props.addMember(newMember);
+      setInput({Name: "", Email: "", Role: "" });
+
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <fieldset>
+                <legend>New Team Member</legend>
+                <label htmlFor='name'>
+                Name:
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        placeholder="name"
+                        value={input.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor='email'>
+                Email:
+                    <input
+                        type='text'
+                        id='email'
+                        name='email'
+                        placeholder="email"
+                        value={input.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label htmlFor='role'>
+                Role:
+                    <input
+                        type='text'
+                        id='email'
+                        name='role'
+                        placeholder="role"
+                        value={input.role}
+                        onChange={handleChange}
+                    />
+                </label>
+
+            </fieldset>
+            <button type='submit'>Submit</button>
+
+        </form>
+
+    );
+}
+
+export default Form;
